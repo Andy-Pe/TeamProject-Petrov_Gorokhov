e6ffec4acfe280adea0eb3a19c433539cad4f698
Andrey Petrov <Pandand1@yandex.ru>
2022-12-04 16:53:44 +0300

    Add logic in method mostPlayerByGenre

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -66,7 +66,16 @@ public class Player {
      * Метод принимает жанр и возвращает игру этого жанра, в которую играли больше всего
      * Если в игры этого жанра не играли, возвращается null
      */
-    public Game mostPlayerByGenre(String genre) {
-        return null;
+    public String mostPlayerByGenre(String genre) {
+        int mostTime = 1;
+        String mostPlayedGame = null;
+        for (Game gameName : playedTime.keySet()) {
+            int playerTime = playedTime.get(gameName);
+            if ((playerTime > mostTime) && (gameName.getGenre().equals(genre))) {
+                mostTime = playerTime;
+                mostPlayedGame = gameName.getTitle();
+            }
+        }
+        return mostPlayedGame;
     }
 }
--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
-
+    
     @Test
     public void shouldSumGenreIfOneGame() {
         GameStore store = new GameStore();
@@ -19,6 +19,6 @@ public class PlayerTest {
         int actual = player.sumGenre(game.getGenre());
         assertEquals(expected, actual);
     }
-
     // другие ваши тесты
 }
+
